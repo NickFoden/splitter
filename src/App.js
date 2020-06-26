d5ecc361be55b1f67c056a1d5546b4a75e2dc73f
Nick Foden <nickfoden@gmail.com>
2020-06-25 21:51:22 -0400

    Messing with the splits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Split from "react-split";
+import SplitPane from "react-split-pane";
+// import One from "./components/One";
+// import Two from "./components/Two";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Split
+    //   sizes={[50, 50]}
+    //   minSize={100}
+    //   // expandToMin={true}
+    //   // gutterSize={10}
+    //   // gutterAlign="center"
+    //   // snapOffset={30}
+    //   dragInterval={50}
+    //   direction="horizontal"
+    //   // cursor="col-resize"
+    // >
+    //   <One />
+    //   <Two />
+    // </Split>
+    <SplitPane split="vertical" minSize={50}>
+      <div />
+      <SplitPane split="horizontal">
+        <div />
+        <div />
+      </SplitPane>
+    </SplitPane>
   );
 }
 
